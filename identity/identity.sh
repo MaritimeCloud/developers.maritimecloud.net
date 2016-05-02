58ddd086e6f3bd852b3812b1f7aebfe9d56a5b1b
Kasper Nielsen <kasperni@gmail.com>
2016-05-02 19:34:12 +0200

    initial commit

---
+++ identity/identity.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$1" = "pdf" ]; then
+   cd adoc
+   asciidoctor-pdf -r asciidoctor-diagram identity.adoc -D ../target
+   cd ..
+   exit
+elif [ "$1" = "html" ]; then
+   asciidoctor -r asciidoctor-diagram adoc/identity.adoc -D target
+   exit
+elif [ "$1" = "site" ]; then
+   echo "Checking for changes to the faq"
+   git status
+   if ! git diff-index --quiet HEAD --; then
+     echo "There are uncommitted changes"
+     exit 1
+   fi
+   asciidoctor -r asciidoctor-diagram adoc/identity.adoc -D target
+#   cp images/*.png target/images
+#   cp images/*.svg target/images
+   git checkout gh-pages
+#   cp target/identity.html ../../identity/index.html
+#   cp target/images/* ../../images/
+   git add ../../identity/index.html
+#   git add ../../images/*
+   git commit -m "update identity documentation"
+   git push
+   git checkout master
+   exit   
+elif [ -z "$1" ]; then 
+	echo Usage: $0 target
+	echo where target is:
+else
+	echo Unknown target: "$1"
+	echo Valid targets are:
+fi
+
+echo "  pdf        Generates documentation in pdf"
+echo "  html       Generates documentation in html"
+
