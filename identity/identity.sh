62eec5821df5a1ce5d723450e88fe410c0de5380
Kasper Nielsen <kasperni@gmail.com>
2016-05-03 10:04:14 +0200

    fix

--- identity/identity.sh
+++ identity/identity.sh
@@ -17,9 +17,9 @@ elif [ "$1" = "site" ]; then
    fi
    asciidoctor -r asciidoctor-diagram adoc/identity.adoc -D target
    cp images/*.png target/images
-#   cp images/*.svg target/images
    git checkout gh-pages
    cp target/identity.html ../identity/index.html
+   mkdir ../identity/images
    cp target/images/* ../identity/images/
    git add ../identity/index.html
    git add ../identity/images/*
